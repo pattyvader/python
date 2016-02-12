b137a0feeca64fa59507d1ba2f116b89a9eea031
pattyvader <pattyloren@gmail.com>
2016-02-12 21:49:04 -0200

    Adds a function that makes the ship fire bullets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -12,10 +13,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,3 +4,8 @@ class Settings():
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.buller_color = 60, 60, 60
+        self.bullets_allowed = 3
