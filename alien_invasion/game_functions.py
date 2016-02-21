32903e7f1a605002c3eddf231e077050594b1ec1
pattyvader <pattyloren@gmail.com>
2016-02-21 11:54:38 -0300

    Adds level up in the game

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -23,6 +23,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
 
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        ai_settings.initialize_dynamic_settings()
+
         pygame.mouse.set_visible(False)
 
         stats.reset_stats()
@@ -66,6 +68,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 
     if len(aliens) == 0:
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,15 +1,33 @@
 class Settings():
     def __init__(self):
+        #Screen settings
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 1
+
+        #Ship settings
+        self.ship_limit = 3
+
+        #Bullets settings
         self.bullet_width = 3
         self.bullet_height = 15
         self.buller_color = 60, 60, 60
         self.bullets_allowed = 3
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
-        self.fleet_direction = 1
-        self.ship_limit = 3
+
+        #Aliens settings
+        self.fleet_drop_speed = 1
+
+        self.speedup_scale = 1.2
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+            self.ship_speed_factor = 1.5
+            self.bullet_speed_factor = 3
+            self.alien_speed_factor = 1
+            self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
