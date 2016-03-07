28f7f533cc083943de6a54092809a260868e2f8e
pattyvader <pattyloren@gmail.com>
2016-03-07 20:09:53 -0300

    Creates the model

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My APPs
+    'learning_logs'
 ]
 
 MIDDLEWARE_CLASSES = [
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
